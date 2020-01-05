5d16f457031c2ed0fd9f03d7c15cd828c0b981f0
Guilherme Belmonte <contact@belmonte.dev>
2020-01-05 00:00:56 -0300

    Change shader files

--- src/vshader.glsl
+++ src/vshader.glsl
@@ -1,7 +1,7 @@
-#version 330 core
+#version 330
 
-in vec2 vPosition;
+layout (location = 0) in vec3 pos;
 
 void main() {
-  gl_Position = vec4(vPosition, 0.0, 1.0);
+  gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
 }
